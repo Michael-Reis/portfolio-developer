02d4235563d79d8fe39821f0edf83901f86129df
Michael-Reis <michael_farias14@hotmail.com>
2025-08-16 22:37:48 -0300

    feat: adicionando carrousel

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,16 +1,37 @@
 import Threads from "../Backgrounds/Threads/Threads";
+import InfiniteScroll from "../Components/InfiniteScroll/InfiniteScroll";
 import "./About.css";
 
 export default function About() {
 
+
+    const items: any[] = [
+        { content: "Text Item 1" },
+        { content: <p>Paragraph Item 2</p> },
+        { content: "Text Item 3" },
+        { content: <p>Paragraph Item 4</p> },
+        { content: "Text Item 5" },
+        { content: <p>Paragraph Item 6</p> },
+        { content: "Text Item 7" },
+        { content: <p>Paragraph Item 8</p> },
+        { content: "Text Item 9" },
+        { content: <p>Paragraph Item 10</p> },
+        { content: "Text Item 11" },
+        { content: <p>Paragraph Item 12</p> },
+        { content: "Text Item 13" },
+        { content: <p>Paragraph Item 14</p> },
+    ];
+
     return (
-        <div style={{ width: '100%', height: 'auto', position: 'relative' }}>
-            <Threads
-                amplitude={2}
-                distance={1.3}
-                enableMouseInteraction={false}
-                color={[1, 1, 3]} // Azul Escuro (Dark Blue in RGB)
-            />
+        <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
+            <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 0 }}>
+                <Threads
+                    amplitude={2}
+                    distance={1.3}
+                    enableMouseInteraction={false}
+                    color={[1, 1, 3]} // Azul Escuro (Dark Blue in RGB)
+                />
+            </div>
             <div className="about">
                 <div className="about-content">
                     <div className="about-me">
@@ -19,13 +40,29 @@ export default function About() {
                         <p>
                             Ofereço uma ampla variedade de serviços digitais para atender às necessidades do seu negócio. Meu objetivo é entregar soluções de alta qualidade, incluindo desenvolvimento de sites, lojas virtuais, sistemas personalizados, aplicativos para Android e iOS, além de consultoria especializada em segurança digital.
                         </p>
-                        <h3>Ei, olha eu aqui</h3>
+                        <div className="profile-container">
+                            <img src="/me/michael.png" alt="Michael Reis" className="profile-picture" />
+                            <div className="profile-info">
+                                <h1>Michael Reis</h1>
+                                <h4>Gerente de TI</h4>
+                            </div>
+                        </div>
+                        <p>
+                            Apaixonado por tecnologia e regras de negócios. Pós-graduado em Arquitetura de Software pela FIAP e formado em Análise e Desenvolvimento de Sistemas, fascinado por leitura, design e segurança. Com mais de 09 anos de experiência em analise e desenvolvimento de soluções escaláveis, integrações, projetos end-to-end, desde o levantamento de requisitos até a entrega e sustentação, aplicando Clean Code, Clean Architecture, Hexagonal Architecture, Design Patterns e práticas de segurança
+                        </p>
+
                     </div>
-                    <div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti incidunt nam quam non laudantium et atque, eaque, cum fugiat, dolor sequi! Accusantium non ab voluptatibus repellat rerum ut blanditiis doloribus.</p>
+                    <div style={{position: 'relative', width: '100%', height: '100%'}}>
+                        <InfiniteScroll
+                            items={items}
+                            isTilted={true}
+                            tiltDirection='right'
+                            autoplay={true}
+                            autoplaySpeed={0.1}
+                        />
                     </div>
 
-                    
+
                 </div>
 
 
